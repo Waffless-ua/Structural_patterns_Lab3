d610ed7034cf32504416f539efc0e660b0ffcb6a
Waffless-ua <wafflesswithoutwaffles@gmail.com>
2026-04-01 18:16:43 +0300

    Task5

---
+++ Structural_patterns/ClassLibrary/Composite/LightElementNode.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Composite
+{
+    public class LightElementNode : LightNode
+    {
+        private string tagName;
+        private bool isBlock;
+        private bool isSelfClosing;
+        private List<string> classes = new List<string>();
+        private List<LightNode> children = new List<LightNode>();
+
+        public LightElementNode(string tagName, bool isBlock, bool isSelfClosing)
+        {
+            this.tagName = tagName;
+            this.isBlock = isBlock;
+            this.isSelfClosing = isSelfClosing;
+        }
+
+        public void AddClass(string className)
+        {
+            classes.Add(className);
+        }
+
+        public void AddChild(LightNode node)
+        {
+            children.Add(node);
+        }
+
+        public int ChildCount()
+        {
+            return children.Count;
+        }
+
+        public override string InnerHTML()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var child in children)
+            {
+                sb.Append(child.OuterHTML());
+            }
+
+            return sb.ToString();
+        }
+
+        public override string OuterHTML()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("<" + tagName);
+
+            if (classes.Count > 0)
+            {
+                sb.Append(" class=\"");
+                sb.Append(string.Join(" ", classes));
+                sb.Append("\"");
+            }
+
+            if (isSelfClosing)
+            {
+                sb.Append(" />");
+                return sb.ToString();
+            }
+
+            sb.Append(">");
+            sb.Append(InnerHTML());
+            sb.Append("</" + tagName + ">");
+
+            return sb.ToString();
+        }
+    }
+}
--- Structural_patterns/ConsoleApp/Program.cs
+++ Structural_patterns/ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using ClassLibrary.Bridge;
 using ClassLibrary.Bridge.Shapes;
 using ClassLibrary.Decorator;
+using ClassLibrary.Composite;
 using ClassLibrary.Decorator.Heros;
 using System.Text;
 
@@ -14,9 +15,10 @@ namespace ConsoleApp
             Console.InputEncoding = Encoding.UTF8;
             Console.OutputEncoding = Encoding.UTF8;
 
-            AdapterPrint();
-            DecoratorPrint();
-            BridgePrint();
+            //AdapterPrint();
+            //DecoratorPrint();
+            //BridgePrint();
+            DemonstrateCompositor();
         }
 
         public static void AdapterPrint()
@@ -76,6 +78,33 @@ namespace ConsoleApp
             anotherTriangle.Draw();
 
             Console.WriteLine();
+        }
+
+        public static void DemonstrateCompositor()
+        {
+            var ul = new LightElementNode("ul", true, false);
+            ul.AddClass("my-list");
+
+            var li1 = new LightElementNode("li", false, false);
+            li1.AddChild(new LightTextNode("Перший елемент"));
+
+            var li2 = new LightElementNode("li", false, false);
+            li2.AddChild(new LightTextNode("Другий елемент"));
+
+            var li3 = new LightElementNode("li", false, false);
+            li3.AddChild(new LightTextNode("Третій елемент"));
+
+            ul.AddChild(li1);
+            ul.AddChild(li2);
+            ul.AddChild(li3);
+
+            Console.WriteLine("OuterHTML:");
+            Console.WriteLine(ul.OuterHTML());
+
+            Console.WriteLine("\nInnerHTML:");
+            Console.WriteLine(ul.InnerHTML());
+
+            Console.WriteLine("\nКількість дочірніх елементів: " + ul.ChildCount());
         }
     }
 }
