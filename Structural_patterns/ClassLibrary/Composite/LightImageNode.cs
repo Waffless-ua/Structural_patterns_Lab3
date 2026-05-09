66d3c0ea96eb226624cd21e09a9fd9439861df84
Waffless-ua <wafflesswithoutwaffles@gmail.com>
2026-05-09 15:06:55 +0300

    Implemented strategy pattern

---
+++ Structural_patterns/ClassLibrary/Composite/LightImageNode.cs
@@ -0,0 +1,48 @@
+﻿using ClassLibrary.Composite.Strategy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Composite
+{
+    public class LightImageNode : LightNode
+    {
+        private string href;
+        private IImageLoadStrategy strategy;
+
+        public LightImageNode(string href)
+        {
+            this.href = href;
+            SetStrategy();
+        }
+
+        private void SetStrategy()
+        {
+            if (href.StartsWith("http"))
+            {
+                strategy = new NetworkImageLoadStrategy();
+            }
+            else
+            {
+                strategy = new FileImageLoadStrategy();
+            }
+        }
+
+        public override string OuterHTML()
+        {
+            return $"<img src=\"{href}\" />";
+        }
+
+        public override string InnerHTML()
+        {
+            return "";
+        }
+
+        public string LoadImage()
+        {
+            return strategy.Load(href);
+        }
+    }
+}
