a6a9e7f340bc3e41996ad619a90e5fa83909326c
Waffless-ua <wafflesswithoutwaffles@gmail.com>
2026-05-09 15:35:07 +0300

    Implemented builder

---
+++ Structural_patterns/ClassLibrary/Composite/Builder/ILightElementBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.Composite.Builder
+{
+    public interface ILightElementBuilder
+    {
+        ILightElementBuilder AddClass(string className);
+        ILightElementBuilder AddChild(LightNode node);
+        LightElementNode Build();
+    }
+}
